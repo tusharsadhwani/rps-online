ea4193420eb4a7342ef6782d90cfb876708ab21b
Tushar Sadhwani <18bit160@ietdavv.edu.in>
2020-01-07 21:58:10 +0530

    added room code generation

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+import random
+import string
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 
@@ -5,7 +7,12 @@ app = Flask(__name__)
 CORS(app)
 app.config["DEBUG"] = True
 
+rooms = []
+
 @app.route('/new')
 def new_game():
-    print(request.args)
-    return jsonify({'success': True})
+    room_code = ''
+    for _ in range(5):
+        room_code += random.choice(string.ascii_letters + string.digits)
+
+    return jsonify({'code': room_code})
